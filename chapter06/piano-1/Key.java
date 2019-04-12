bb3aae2036bc48daff164ecae501884d1d96402f
Peter Zhang <szhang@lcs.on.ca>
2019-04-12 14:45:52 -0400

    Get key press workiing

--- chapter06/piano-1/Key.java
+++ chapter06/piano-1/Key.java
@@ -8,18 +8,36 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Key extends Actor
 {
+    
+    /**
+     * Instence varible
+     */
+    //Tharks the piano key is down or not
+    private boolean isDown;
+    
+    
+    /**
+     * Creat new key
+     */
+    public Key()
+    {
+       isDown = false;
+    }
+    
+    
     /**
      * Act - do whatever the Key wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        if( Greenfoot.isKeyDown("g"))
+        if(! isDown == false && Greenfoot.isKeyDown("g"))
         {
             setImage("white-key-down.png");
+            isDown = true;
         }    
 
-        else
+        if (isDown == true && Greenfoot.isKeyDown("g") == false)
         {
 
             setImage("white-key.png");
