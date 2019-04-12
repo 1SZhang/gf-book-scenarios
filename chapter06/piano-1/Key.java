a4184fda3ef9dd6e210b907ea0d34f30fb9dd997
Peter Zhang <szhang@lcs.on.ca>
2019-04-12 14:34:36 -0400

    got key pressing

--- chapter06/piano-1/Key.java
+++ chapter06/piano-1/Key.java
@@ -1,19 +1,29 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
+/**
+ * Write a description of class Key here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
 public class Key extends Actor
 {
     /**
-     * Create a new key.
+     * Act - do whatever the Key wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public Key()
+    public void act() 
     {
-    }
+        if( Greenfoot.isKeyDown("g"))
+        {
+            setImage("white-key-down.png");
+        }    
 
-    /**
-     * Do the action for this key.
-     */
-    public void act()
-    {
+        else
+        {
+
+            setImage("white-key.png");
+
+        }
     }
 }
-
